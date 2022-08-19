793d87bc3af98978fcdaf3f565b872d51d8572b0
rrezartveseli1 <101754699+rrezartveseli1@users.noreply.github.com>
2022-08-19 15:36:52 +0200

    Add files via upload

---
+++ uk.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+// set up the request parameters
+const params = {
+api_key: "E3078FB9F8764BE5BE244180F14F3C33",
+  q: "bitrefill",
+  location: "United Kingdom"
+}
+
+// make the http GET request to Scale SERP
+axios.get('https://api.scaleserp.com/search', { params })
+.then(response => {
+
+    // print the JSON response from Scale SERP
+    console.log(JSON.stringify(response.data, 0, 1));
+
+  }).catch(error => {
+// catch and print the error
+console.log(error);
+})
